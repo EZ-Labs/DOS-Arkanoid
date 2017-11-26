1e9c9de9786ece29f9cb7d761cd2686eb39ce43d
EZSPECIAL <EZSPECIAL@users.noreply.github.com>
2017-11-26 03:51:33 +0000

    Custom image filetype RLE compression

---
+++ SPRITE/RAW2LCA.C
@@ -0,0 +1,55 @@
+#include <STDIO.H>
+#include <LIMITS.H>
+#include "MYTYPES.H"
+
+int main(int argc, char** argv) {
+	
+	int16_t data = 0;
+	int16_t previous_data = 0;
+	uint8_t color_counter = 0; /* Current count of repeating bytes from file */
+	FILE* raw_file;
+	FILE* lca_file;
+	
+	/* Create filepath for compressed file */
+	char* filepath = (char*) malloc(strlen(argv[1]) + 5);
+	strcpy(filepath, argv[1]);
+	strcat(filepath, "_lca");
+	
+	/* Open file for compressing and create destination file */
+	raw_file = fopen(argv[1], "rb");
+	lca_file = fopen(filepath, "w");
+	
+	if(raw_file == NULL) {
+		
+		printf("File %s doesn't exist!\n", argv[1]);
+		return 1;
+	}
+	
+	data = getc(raw_file);
+	color_counter++;
+	
+	/* Read file until EOF reached, writes compressed blocks when different byte value appears */
+	while(data != EOF) {
+		
+		(uint8_t) previous_data = data;
+		data = getc(raw_file);
+		
+		if(previous_data == data) {
+			color_counter++;
+			if(color_counter == UCHAR_MAX) {
+				fwrite(&previous_data, sizeof(uint8_t), 1, lca_file);
+				fwrite(&color_counter, sizeof(uint8_t), 1, lca_file);
+				color_counter = 1;
+			}
+		} else {
+			fwrite(&previous_data, sizeof(uint8_t), 1, lca_file);
+			fwrite(&color_counter, sizeof(uint8_t), 1, lca_file);
+			color_counter = 1;
+		}
+	}
+	
+	fclose(raw_file);
+	fclose(lca_file);
+	
+	return 0;
+}
