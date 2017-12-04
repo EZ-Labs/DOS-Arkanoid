51c06d0ba4a86e726862e8008689411f34e17318
EZSPECIAL <ftlopes93@gmail.com>
2017-12-04 12:20:40 +0000

    Pixel paint test

--- PAINT/PAINT.C
+++ PAINT/PAINT.C
@@ -10,47 +10,61 @@
 
 uint8_t mouseGetButtons() {
 	
-  union REGS regs;
-  
-  regs.x.ax = 0x03;
-  int86(0x33, &regs, &regs);
-  
-  return regs.h.bl;
+	union REGS regs;
+	
+	regs.x.ax = 0x03;
+	int86(0x33, &regs, &regs);
+	
+	system("CLS");
+	printf("x: %d y: %d\n", regs.x.cx, regs.x.dx);
+	
+	return regs.h.bl;
+}
+
+void mousePaint() {
+	
+	union REGS regs;
+	
+	regs.x.ax = 0x03;
+	int86(0x33, &regs, &regs);
+	
+	vg_pixel(regs.x.cx, regs.x.dx, 0x02);
 }
 
 void mouseShowCursor() {
 	
-  union REGS regs;
-  
-  regs.x.ax = 0x01;
-  int86(0x33, &regs, &regs);
+	union REGS regs;
+	
+	regs.x.ax = 0x01;
+	int86(0x33, &regs, &regs);
 }
 
 void mouseHideCursor() {
 	
-  union REGS regs;
-  
-  regs.x.ax = 0x02;
-  int86(0x33, &regs, &regs);
+	union REGS regs;
+	
+	regs.x.ax = 0x02;
+	int86(0x33, &regs, &regs);
 }
 
 int main(int argc, char** argv) {
 	
 	uint8_t buttonStatus;
 	
-	vgaSetMode(VGA256);
+	/*vgaSetMode(VGA256);*/
 	
 	mouseShowCursor();
 	
 	do {
 		
 		buttonStatus = mouseGetButtons();
+		if(buttonStatus & RB_PRESS) mousePaint();
 		
 	} while(!(buttonStatus & LB_PRESS));
 	
 	mouseHideCursor();
 	
-	vgaSetMode(VGA256);
+	/*vgaSetMode(TEXT);*/
 	
 	return 0;
 }
--- PAINT/VGA.C
+++ PAINT/VGA.C
@@ -2,6 +2,11 @@
 #include <DOS.H>
 #include "VGA.H"
 
+static char far* video_mem = (char far*) 0xA0000000L;
+static unsigned short h_res = 320;
+static unsigned short v_res = 200;
+static char bits_per_pixel = 8;
+
 /* Uses BIOS int 10h to set video mode (no VESA Modes) */
 void vgaSetMode(char mode) {
 	
@@ -10,4 +15,14 @@ void vgaSetMode(char mode) {
   regs.h.ah = 0x00;
   regs.h.al = mode;
   int86(0x10, &regs, &regs);
+}
+
+void vg_pixel(unsigned short x, unsigned short y, unsigned long color) {
+	
+	char far* vptr = video_mem;
+	
+	if(x > h_res - 1 || y > v_res - 1) return;
+
+	vptr += x + y * h_res;
+	*vptr = color & 0xFF;
 }
